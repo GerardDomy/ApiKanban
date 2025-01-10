8c36e449ed4a4acd4179b83822206c7f59410204
GerardDomy <182240599+GerardDomy@users.noreply.github.com>
2025-01-10 15:25:11 +0100

    Add files via upload

---
+++ WpfAppTestAPIClient/WpfAppTestAPIClient/APIClient/TascasApiClient.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppTestAPIClient.Model;
+using System.Configuration;
+
+namespace WpfAppTestAPIClient.APIClient
+{
+    public class TascasApiClient
+    {
+        string BaseUri;
+        
+        
+        public TascasApiClient()
+        {
+            BaseUri = ConfigurationManager.AppSettings["BaseUri"];
+        }
+        
+        /// <summary>
+        /// Obté un usuari a partir del Id
+        /// </summary>
+        /// <param name="Autor">Codi d'usuari</param>
+        /// <returns>Usuari o null si no el troba</returns>
+        public async Task<Tasca> GetUserAsync(string Autor)
+        {
+            Tasca tasca = new Tasca();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició GET al endpoint /users/{Id}
+                HttpResponseMessage response = await client.GetAsync($"tascas/{Autor}");
+                if (response.IsSuccessStatusCode)
+                {
+                    //Reposta 204 quan no ha trobat dades
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        tasca = null;
+                    }
+                    else
+                    {
+                        //Obtenim el resultat i el carreguem al Objecte User
+                        tasca = await response.Content.ReadAsAsync<Tasca>();
+                        response.Dispose();
+                    }
+                }
+                else
+                {
+                    //TODO: que fer si ha anat malament? retornar null? 
+                }
+            }
+            return tasca;
+        }
+
+        /// <summary>
+        /// Obté una llista de tots els usuaris de la base de dades
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Tasca>> GetUsersAsync()
+        {
+            List<Tasca> tascas = new List<Tasca>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició GET al endpoint /users}
+                HttpResponseMessage response = await client.GetAsync("tascas");
+                if (response.IsSuccessStatusCode)
+                {
+                    //Obtenim el resultat i el carreguem al objecte llista d'usuaris
+                    tascas = await response.Content.ReadAsAsync<List<Tasca>>();
+                    response.Dispose();
+                }
+                else
+                {
+                    //TODO: que fer si ha anat malament? retornar null? missatge?
+                }
+            }
+            return tascas;
+        }
+
+        /// <summary>
+        /// Afegeix un nou usuari
+        /// </summary>
+        /// <param name="tasca">Usuari que volem afegir</param>
+        /// <returns></returns>
+        public async Task AddAsync(Tasca tasca)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició POST al endpoint /users}
+                HttpResponseMessage response = await client.PostAsJsonAsync("tascas", tasca);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+
+        /// <summary>
+        /// Modificar un usuari
+        /// </summary>
+        /// <param name="tasca">Usuari que volem modificar</param>
+        /// <returns></returns>
+        public async Task UpdateAsync(Tasca tasca)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició PUT al endpoint /users/Id
+                HttpResponseMessage response = await client.PutAsJsonAsync($"users/{tasca.Autor}", tasca);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+
+
+        /// <summary>
+        /// Modificar un usuari
+        /// </summary>
+        /// <param name="user">Usuari que volem modificar</param>
+        /// <returns></returns>
+        public async Task DeleteAsync(string Autor)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició DELETE al endpoint /users/Id
+                HttpResponseMessage response = await client.DeleteAsync($"tascas/{Autor}");
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
---
+++ WpfAppTestAPIClient/WpfAppTestAPIClient/Model/Tasca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTestAPIClient.Model
+{
+    public class Tasca
+    {
+        public string Nom { get; set; }
+        public string Descripcio { get; set; }
+        public string Autor { get; set; }
+        public DateTime DataInici { get; set; }
+        public DateTime DataFinal { get; set; }
+        public string Estat {  get; set; }
+        public string Background { get; set; }
+    }
+
+}
